5196a9110f3549df796d7e4344db96e9763ea902
roudingbaozi <wudi2017quebec@gmail.com>
2019-02-28 20:39:43 -0500

    test

---
+++ Sum.java
@@ -0,0 +1,13 @@
+
+public class Sum {
+	public static int Sum(int n) {
+		return n*(1 + n)/2;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 1000;
+		System.out.println(Sum(n));
+	}
+
+}
